5fe5f140e2521b03c4b447983f9cd770859e39b3
Simona Stoica <symonastoica@gmail.com>
2022-06-03 13:49:06 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -51,13 +51,24 @@ class Dev(Configuration):
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "django.contrib.sites",
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
     'blango_auth',
     'blog',
-    'debug_toolbar'    
+    'debug_toolbar',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google'
     ]
+
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
--- blango/urls.py
+++ blango/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path('post/<slug>/', blog.views.post_detail, name='blog-post-detail'),
     path("ip/", blog.views.get_ip)
 ]
